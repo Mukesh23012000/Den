01660639f4c03baee5dd5598c74449a505660bd5
Mukesh <sendtomk1@gmail.com>
2022-11-19 00:05:24 +0530

    first commit

--- AI/views.py
+++ AI/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import os
 import openai
-openai.api_key = "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"
+openai.api_key = "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"
 respons = openai.Completion.create(
   engine="text-davinci-002",
   prompt="Write a Random Quote",
