bbe9c06e3998409710eada2a2374f099bc78512a
Mukesh <70316270+Mukesh23012000@users.noreply.github.com>
2022-11-22 15:59:19 +0530

    Update urls.py

--- AIprob/urls.py
+++ AIprob/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from AI import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index,name='index'),
+    path('index',views.index,name='index'),
+    path('GC',views.GC,name='GC'),
+    path('emoji',views.emoji,name='emoji'),
+    path('quest',views.quest,name='quest'),
+    path('quote',views.quote,name='quote'),
     path('lst',views.lst),
 ]
